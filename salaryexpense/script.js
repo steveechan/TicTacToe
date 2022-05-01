d4bc5cab76bad621ef551f10504288b7aa92e44a
stephen <liluzisteveee@gmail.com>
2022-05-01 18:54:59 -0400

    calculate function completed

--- salaryexpense/script.js
+++ salaryexpense/script.js
@@ -1,3 +1,10 @@
-let hours = document.getElementByName("day");
+function calculate() {
+    const form = document.getElementById('form');
+    var sum = 0;
+    for(let i = 0; i < form.length; i++) {
+        sum += parseInt(form[i].value);
+    }
+    var hourlyPay = document.getElementById("hourly").value;
+        var result = sum * hourlyPay;
+    }
 
-let hourlyPay = document.getElementById("hourly");
