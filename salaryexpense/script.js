1eef2fd990addc2425bcbf5de3849914dbedd705
stephen <liluzisteveee@gmail.com>
2022-05-02 00:01:33 -0400

    button changes

--- salaryexpense/script.js
+++ salaryexpense/script.js
@@ -1,10 +1,33 @@
-function calculate() {
+function weekly() {
     const form = document.getElementById('form');
-    var sum = 0;
+    let sum = 0;
     for(let i = 0; i < form.length; i++) {
+        if(form[i].value === "") {
+            continue;
+        }
+        else {
         sum += parseInt(form[i].value);
+        }
     }
-    var hourlyPay = document.getElementById("hourly").value;
-        var result = sum * hourlyPay;
+    let hourlyPay = document.getElementById("hourly").value;
+        let result = sum * hourlyPay;
+
+    let p = document.getElementById("amountweekly");
+    p.innerText = "\n" + "The amount you made this week is $" + result;
+    p.style.display = "block";
     }
+
+function daily() {
+
+    let dailyHourlyPay = document.getElementById("dailyhourly").value;
+    let hours = document.getElementById("dailytoday").value;
+    if(hours === "") {
+        hours = 0;
+    }
+    let todayWork = dailyHourlyPay * parseInt(hours);
+
+    let p2 = document.getElementById("amountdaily");
+    p2.innerText = "\n" + "The amount you made today is $" + todayWork;
+    p2.style.display = "block";
+}
 
